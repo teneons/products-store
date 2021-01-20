0e5dcc77982521809318f55c82b46645a448330b
teneons <teneons@gmail.com>
2021-01-20 15:30:12 +0200

    corrected data at sign up

--- models/ModelUser.js
+++ models/ModelUser.js
@@ -4,8 +4,7 @@ const schema = new Schema({
   firstName: {type: String, required: true, unique: true},
   lastName: {type: String, required: true, unique: true},
   email: {type: String, required: true, unique: true},
-  password: {type: String, required: true},
-  links: [{type: Types.ObjectId, ref: 'Link'}]
+  password: {type: String, required: true}
 })
 
 module.exports = model('User', schema)
